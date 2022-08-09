107468cbf4d792f2c4bc301112fa462268415198
Pungaz <milanovic.bogdan@gmail.com>
2022-08-09 20:46:53 +0200

    Refined pbp-service

---
+++ pbp-service/src/main/java/rs/edu/raf/msa/pbp/controller/GameController.java
@@ -0,0 +1,61 @@
+package rs.edu.raf.msa.pbp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import rs.edu.raf.msa.pbp.model.Play;
+import rs.edu.raf.msa.pbp.model.PlayByPlay;
+import rs.edu.raf.msa.pbp.services.GameService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+public class GameController {
+
+    private final GameService gameService;
+
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @GetMapping("/game/{gameId}")
+    public PlayByPlay game(@PathVariable("gameId") String gameId) {
+        return gameService.game(gameId);
+    }
+
+
+    @GetMapping("/plays/{gameId}/{fromMin}/{toMin}")
+    public List<Play> plays(@PathVariable("gameId") String gameId,
+                            @PathVariable("fromMin") String fromMin,
+                            @PathVariable("toMin") String toMin) {
+        PlayByPlay game = gameService.game(gameId);
+        return game.play(fromMin, toMin);
+    }
+
+    @GetMapping("/games")
+    public List<String> games() {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver(classLoader);
+        Resource[] games;
+        try {
+            games = resolver.getResources("classpath:games/*.json");
+
+            List<String> result = new ArrayList<>(games.length);
+            for (Resource g : games) {
+                result.add(Objects.requireNonNull(g.getFilename()).replace(".json", ""));
+            }
+            return result;
+        } catch (IOException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error opening games!");
+        }
+    }
+
+}
---
+++ pbp-service/src/main/java/rs/edu/raf/msa/pbp/model/PlayByPlay.java
@@ -0,0 +1,60 @@
+package rs.edu.raf.msa.pbp.model;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+public class PlayByPlay {
+
+    Map<String, Player> players = new LinkedHashMap<>();
+
+    List<Quarter> quarters = new ArrayList<>(4);
+
+    public List<Play> play(String fromMinString, String toMinString) {
+        double fromMin = fromStringToDouble(fromMinString);
+        double toMin = fromStringToDouble(toMinString);
+
+        List<Play> result = new ArrayList<>();
+        for (Quarter quarter : quarters) {
+            List<Play> plays = quarter.getPlays();
+            for (Play play : plays) {
+                if (currentTime(quarter.q, play.c) >= fromMin && currentTime(quarter.q, play.c) <= toMin) {
+                    result.add(play);
+                }
+            }
+        }
+        return result;
+    }
+
+    public double fromStringToDouble(String string) {
+        try {
+            String[] stringArray = string.split(":");
+            return Double.parseDouble(stringArray[0]) + Double.parseDouble(stringArray[1]) / 100;
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error with entered time period", e);
+        }
+    }
+
+    public double currentTime(int quarter, String currentTime) {
+        return ((quarter - 1) * 12) + fromStringToDouble(currentTime) - 12.0;
+    }
+
+    public List<Player> players() {
+        List<Player> players = new ArrayList<>();
+
+        for (Map.Entry<String, Player> entry : getPlayers().entrySet()) {
+            Player player = entry.getValue();
+//            player.setExternalId(entry.getKey());
+            players.add(player);
+        }
+        //return new ArrayList<>(getPlayers().values());
+        return players;
+    }
+}
