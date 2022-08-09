c512cbf4f67cfc997df77f70819b2e098be21042
Pungaz <milanovic.bogdan@gmail.com>
2022-08-09 18:58:57 +0200

    Refined pbp-service

--- pbp/src/main/java/rs/edu/raf/msa/pbp/model/PlayByPlay.java
+++ pbp/src/main/java/rs/edu/raf/msa/pbp/model/PlayByPlay.java
@@ -1,8 +1,13 @@
 package rs.edu.raf.msa.pbp.model;
 
 import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 @Data
 public class PlayByPlay {
@@ -12,50 +17,36 @@ public class PlayByPlay {
     List<Quarter> quarters = new ArrayList<>(4);
 
     public List<Play> play(String fromMinString, String toMinString) {
-
         double fromMin = fromStringToDouble(fromMinString);
         double toMin = fromStringToDouble(toMinString);
 
-        int fromQuarter = getQuarter(fromMin);
-        int toQuarter = getQuarter(toMin);
-
         List<Play> result = new ArrayList<>();
-        for(Quarter quarter : quarters) {
+        for (Quarter quarter : quarters) {
             List<Play> plays = quarter.getPlays();
             for (Play play : plays) {
-                if (currentTime(quarter.q, play.c) >= fromMin && currentTime(quarter.q, play.c) <= toMin){
+                if (currentTime(quarter.q, play.c) >= fromMin && currentTime(quarter.q, play.c) <= toMin) {
                     result.add(play);
                 }
             }
         }
-
         return result;
     }
 
-    public int getQuarter(double minute) {
-        if (minute >= 0 && minute <= 12) {
-            return 1;
-        } else if (minute >= 13 && minute <= 24) {
-            return 2;
-        } else if (minute >= 25 && minute <= 36) {
-            return 3;
-        } else if (minute >= 37 && minute <= 48) {
-            return 4;
+    public double fromStringToDouble(String string) {
+        try {
+            String[] stringArray = string.split(":");
+            return Double.parseDouble(stringArray[0]) + Double.parseDouble(stringArray[1]) / 100;
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error with entered time period", e);
         }
-        return 0;
     }
 
-    public double fromStringToDouble(String string){
-        String[] stringArray = string.split(":");
-        return Double.parseDouble(stringArray[0]) + Double.parseDouble(stringArray[1]) / 100;
-    }
-
-    public double currentTime(int quarter, String currentTime){
+    public double currentTime(int quarter, String currentTime) {
         return ((quarter - 1) * 12) + fromStringToDouble(currentTime) - 12.0;
     }
 
-    public List<Player> players(){
-
+    public List<Player> players() {
         List<Player> players = new ArrayList<>();
 
         for (Map.Entry<String, Player> entry : getPlayers().entrySet()) {
@@ -66,5 +57,4 @@ public class PlayByPlay {
         //return new ArrayList<>(getPlayers().values());
         return players;
     }
-
 }
